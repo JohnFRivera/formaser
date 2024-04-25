88cb76c13f6f6371d4669df32bd1bcbe7cf0636f
John <johnfrivera1309@gmail.com>
2024-04-25 07:54:37 -0500

    Solucion de nombre de variables

--- front/admin/dashboard/cargar_archivos/matriculados.php
+++ front/admin/dashboard/cargar_archivos/matriculados.php
@@ -70,7 +70,7 @@
                                     </div>
                                 </div>
                                 <div class="col col-sm-6 col-md-5 col-lg-4 col-xl-3 ms-auto px-0 d-flex flex-column justify-content-start">
-                                    <label for="inpArchivoPreinscrito" class="btn btn-azul fw-semibold px-4">
+                                    <label for="inpArchivoMatriculados" class="btn btn-azul fw-semibold px-4">
                                         <input type="file" class="visually-hidden" name="archivo-inscrito" id="inpArchivoMatriculados" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" required>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mb-1 me-1" viewBox="0 0 16 16">
                                             <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
